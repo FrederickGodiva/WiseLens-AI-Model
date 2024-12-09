7a63b5479ac2f722a208ae25beca70275e296850
FrederickGodiva <godivafrederick@gmail.com>
2024-12-09 23:43:34 +0700

    chore: add nutritions dataset and returning nutritions in api response

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import pandas as pd
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import os
@@ -8,6 +9,7 @@ app = Flask(__name__)
 CORS(app)
 
 class_labels = ["Bakso", "Gado-gado", "Gudeg", "Rendang", "Sate"]
+nutrition_data = pd.read_csv("nutritions.csv")
 
 UPLOAD_FOLDER = "uploads"
 if not os.path.exists(UPLOAD_FOLDER):
@@ -15,7 +17,6 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 @app.route('/predict-cnn', methods=['POST'])
 def predict_cnn():
-    model_path = "model-cnn.keras"
     try:
         print("Request received")
         if 'image' not in request.files:
@@ -25,10 +26,7 @@ def predict_cnn():
         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(file_path)
 
-        print(f"File saved at: {file_path}")
-
-        model = load_model(model_path)
-        print("Model loaded")
+        model = load_model("model-cnn.keras")
 
         img = load_img(file_path, target_size=(80, 120))
         img_array = img_to_array(img) / 255.0
@@ -39,9 +37,19 @@ def predict_cnn():
         predicted_label = class_labels[predicted_index]
         confidence = predictions[0][predicted_index]
 
+        print(predicted_label)
+
+        nutrition = nutrition_data[nutrition_data['name'].str.lower().str.strip() == predicted_label.lower().strip()]
+        if nutrition.empty:
+            return jsonify({"error": f"Nutrition data not found for {predicted_label}"}), 404
+        
+        nutrition_dict = nutrition.iloc[0].to_dict()
+
+
         return jsonify({
             "predicted_label": predicted_label,
-            "confidence": float(confidence)
+            "confidence": float(confidence),
+            "nutritions": nutrition_dict
         })
 
     except Exception as e:
@@ -51,17 +59,18 @@ def predict_cnn():
 
 @app.route('/predict-pretrained', methods=['POST'])
 def predict_pretrained():
-    model_path = "model-pretrained.keras"
-    model = load_model(model_path)
     try:
-        image_path = request.json.get("image")
-        if not image_path:
-            return jsonify({"error": "Image path is required"}), 400
-        
-        if not os.path.exists(image_path):
-            return jsonify({"error": f"File not found: {image_path}"}), 404
+        print("Request received")
+        if 'image' not in request.files:
+            return jsonify({"error": "No file uploaded"}), 400
 
-        img = load_img(image_path, target_size=(80, 120))
+        file = request.files['image']
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(file_path)
+
+        model = load_model("model-pretrained.keras")
+
+        img = load_img(file_path, target_size=(80, 120))
         img_array = img_to_array(img) / 255.0
         img_array = img_array.reshape((1,) + img_array.shape)
 
@@ -70,12 +79,23 @@ def predict_pretrained():
         predicted_label = class_labels[predicted_index]
         confidence = predictions[0][predicted_index]
 
+        print(predicted_label)
+
+        nutrition = nutrition_data[nutrition_data['name'].str.lower().str.strip() == predicted_label.lower().strip()]
+        if nutrition.empty:
+            return jsonify({"error": f"Nutrition data not found for {predicted_label}"}), 404
+        
+        nutrition_dict = nutrition.iloc[0].to_dict()
+
+
         return jsonify({
             "predicted_label": predicted_label,
-            "confidence": float(confidence)
+            "confidence": float(confidence),
+            "nutritions": nutrition_dict
         })
 
     except Exception as e:
+        print(f"Error: {e}")
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
